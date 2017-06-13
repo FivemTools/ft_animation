5e1a2d2c88354231630e900c0f2742119585d8d4
samuelds <samuelds.pro@gmail.com>
2017-06-13 18:34:18 +0200

    Update

--- cl_functions.lua
+++ cl_functions.lua
@@ -1,9 +1,10 @@
 -- @Date:   2017-06-12T16:44:10+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-13T11:15:06+02:00
+-- @Last modified time: 2017-06-13T17:36:23+02:00
 -- @License: GNU General Public License v3.0
 
 local playAnim = false
+local dataAnim = {}
 
 function AnimAction(data)
   Citizen.CreateThread(function()
@@ -11,28 +12,41 @@ function AnimAction(data)
     if not playAnim then
 
       local playerPed = GetPlayerPed(-1);
+      if DoesEntityExist(playerPed) then -- Ckeck if ped exist
 
-      -- Play Animation
-      RequestAnimDict( data.lib )
-      while not HasAnimDictLoaded( data.lib ) do
-        Citizen.Wait(0)
-      end
-      if HasAnimDictLoaded( data.lib ) then
-        TaskPlayAnim( playerPed, data.lib , data.anim , 8.0, -8.0, -1, 0, 0, false, false, false )
-        playAnim = true
-      end
+        dataAnim = data
 
-      -- Wait end annimation
-      while true do
+        -- Play Animation
+        RequestAnimDict(data.lib)
+        while not HasAnimDictLoaded(data.lib) do
+          Citizen.Wait(0)
+        end
+        if HasAnimDictLoaded(data.lib) then
 
-        Citizen.Wait(0)
-        if not IsEntityPlayingAnim(playerPed, data.lib, data.anim, 3) then
-          playAnim = false
-          TriggerEvent('ft_animation:ClFinish')
-          break
+          local flag = 0
+          if data.loop ~= nil and data.loop then
+            flag = 1
+          elseif data.move ~= nil and data.move then
+            flag = 49
+          end
+
+          TaskPlayAnim(playerPed, data.lib, data.anim, 8.0, -8.0, -1, flag, 0, 0, 0, 0)
+          playAnimation = true
         end
 
-      end
+        -- Wait end annimation
+        while true do
+
+          Citizen.Wait(0)
+          if not IsEntityPlayingAnim(playerPed, data.lib, data.anim, 3) then
+            playAnim = false
+            TriggerEvent('ft_animation:ClFinish')
+            break
+          end
+
+        end
+
+      end -- end ped exist
 
     else
       TriggerEvent('ft_animation:ClPending')
@@ -47,8 +61,11 @@ function AnimActionScenario(data)
     if not playAnim then
 
       local playerPed = GetPlayerPed(-1);
-      if playerPed then
-        TaskStartScenarioInPlace( playerPed, data.anim, 0, false )
+      if DoesEntityExist(playerPed) then
+
+        TaskStartScenarioInPlace(playerPed, data.anim, 0, false)
+
+        dataAnim = data
         playAnimation = true
       end
 
@@ -61,14 +78,22 @@ end
 
 function AnimStop()
   Citizen.CreateThread(function()
-
     if playAnimation then
-
       local playerPed = GetPlayerPed(-1);
-      if playerPed then
+      if DoesEntityExist(playerPed) then
+
+        if dataAnim.lib ~= nil then
+          StopEntityAnim(playerPed, dataAnim.anim, dataAnim.lib, 3)
+        end
+
         ClearPedTasks(playerPed)
+        ClearPedSecondaryTask(playerPed)
+
         playAnimation = false
+        dataAnim = {}
+
         TriggerEvent('ft_animation:ClStopped')
+
       end
 
     end
@@ -83,9 +108,11 @@ function AnimForceStop()
 
       local playerPed = GetPlayerPed(-1);
       if playerPed then
+
         ClearPedTasksImmediately(playerPed)
-        playAnimation = false
         TriggerEvent('ft_animation:ClStopped')
+
+        playAnimation = false
       end
 
     end
